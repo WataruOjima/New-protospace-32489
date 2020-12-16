e40fd6fed3f57492d4b3bfd4ad193513c95b62b8
WataruOjima <wataru.ojima20@gmail.com>
2020-12-16 18:06:58 +0900

    まとめて
    投稿したプロトタイプをトップページに表示
    削除機能まで

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,17 +3,41 @@ class PrototypesController < ApplicationController
     @prototypes = Prototype.all
   end
 
-  def  new
+  def new
     @prototype = Prototype.new
   end
 
   def create
-    @prototype = Prototype.new (prototype_params)
+    @prototype = Prototype.new(prototype_params)
     if @prototype.save
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  def show 
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
   end
 
   private
