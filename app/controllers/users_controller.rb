a2cb79e6b84fcd5a76ea06432f3c73dcd0ecd5fe
WataruOjima <wataru.ojima20@gmail.com>
2020-12-17 21:25:46 +0900

    投稿者以外のユーザーが、投稿者専用のページに遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
-
+  before_action :user_signed_in
   def show
     @user = User.find(params[:id])
     @name = @user.name
     @prototypes = @user.prototypes
+  end
+
+  def user_signed_in
+    unless user_signed_in?
+    redirect_to action: :index
+    end
   end
 
 end
